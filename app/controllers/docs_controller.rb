c7ef71e578ac12f783245234156363d508ba8baf
Anirudh Kulkarni <k.anirudh8@gmail.com>
2017-01-15 16:04:18 +0530

    Add styling to welcome and Application.html.erb file

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,7 @@ class DocsController < ApplicationController
 	before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@docs = Doc.all.where(user_id: current_user).order("created_at DESC")
+		@docs = current_user.docs.order("created_at DESC")
 	end
 
 	def show
